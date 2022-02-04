ec50822fc7630b86221f8e45d57734587c17a46d
KWADJOOWUSU-ANSAHQUARSHIE <97170718+KWADJOOWUSU-ANSAHQUARSHIE@users.noreply.github.com>
2022-02-04 01:05:20 +0000

    Add files via upload

---
+++ lesson4/js/date.js
@@ -0,0 +1,9 @@
+const date = new Date();
+const year = date.getFullYear();
+
+
+document.querySelector("#year").textContent = year;
+document.getElementById("updated").textContent = `Last Updated: ${document.lastModified}`;
+
+
+
