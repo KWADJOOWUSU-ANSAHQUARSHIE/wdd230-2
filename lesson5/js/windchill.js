4360437b3de60687cb9f43219129ab73a3f3fa5e
KWADJOOWUSU-ANSAHQUARSHIE <97170718+KWADJOOWUSU-ANSAHQUARSHIE@users.noreply.github.com>
2022-02-25 03:10:31 +0000

    Update

--- lesson5/js/windchill.js
+++ lesson5/js/windchill.js
@@ -5,7 +5,7 @@ let tempCelsius = document.querySelector('.temp-Cels');
 let windSpeed_ = document.querySelector('.wind-speed');
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?id=' +5660340+ '&appid  =44af6e0267e626fb09390194949d7a28')
+fetch('https://api.openweathermap.org/data/2.5/weather?id=' +5660340+ '&appid  =44af6e0267e626fb09390194949d7a28a')
     .then(response => response.json())
     .then(data => {
         let tempValue = data['main']['temp'];
